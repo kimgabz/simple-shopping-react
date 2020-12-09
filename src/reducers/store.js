9854b2adaa88f2243951c2f2887b35822568d418
kim <khemgabz@gmail.com>
2020-12-09 13:10:20 +0800

    order component

--- src/reducers/store.js
+++ src/reducers/store.js
@@ -2,13 +2,15 @@ import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import { productsReducer } from "./products/product.reducer";
 import { cartReducer } from "./cart/cart.reducer";
+import { orderReducer } from "./order/order.reducer";
 
 const initialState = {};
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   combineReducers({
     products: productsReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    order: orderReducer,
   }),
   initialState,
   composeEnhancer(applyMiddleware(thunk))
